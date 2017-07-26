f6adfc16ab97a12abaff7ef89a2700ae738cf55b
MiningMark48 <miningmark48@gmail.com>
2017-07-26 19:19:41 -0400

    Removes repackaging of COFH RF API
    Closes #1

--- src/main/java/com/miningmark48/mininglib/MiningLib.java
+++ src/main/java/com/miningmark48/mininglib/MiningLib.java
@@ -13,7 +13,7 @@ public class MiningLib {
 
     public static final String MOD_ID = "mininglib";
     public static final String MOD_NAME = "MiningLib";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
