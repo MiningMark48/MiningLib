ad4c7b34b318fff24864cbc7f3b72415787a3e50
MiningMark48 <miningmark48@gmail.com>
2017-09-13 00:07:54 -0400

    1.12.1 Port

--- src/main/java/com/miningmark48/mininglib/MiningLib.java
+++ src/main/java/com/miningmark48/mininglib/MiningLib.java
@@ -13,7 +13,7 @@ public class MiningLib {
 
     public static final String MOD_ID = "mininglib";
     public static final String MOD_NAME = "MiningLib";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.1.0";
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
